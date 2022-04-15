7e7a7327f2828732f74c96a80a543a1752b98824
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-04-15 17:07:26 +0300

    Auto commit

---
+++ TSI_get_NOAA.R
@@ -0,0 +1,85 @@
+#!/usr/bin/env Rscript
+# /* Copyright (C) 2022 Athanasios Natsis <natsisthanasis@gmail.com> */
+
+#### Prepare TSI data form NOAA
+
+
+rm(list = (ls()[ls() != ""]))
+Sys.setenv(TZ = "UTC")
+options("width" = 130)
+tic <- Sys.time()
+Script.Name <- tryCatch({ funr::sys.script() },
+                        error = function(e) { cat(paste("\nUnresolved script name: ", e),"\n")
+                            return("Undefined R script name!!") })
+if(!interactive()) {
+    pdf(  file = paste0("~/TSI/REPORTS/", basename(sub("\\.R$",".pdf", Script.Name))))
+    sink( file = paste0("~/TSI/REPORTS/", basename(sub("\\.R$",".out", Script.Name))), split = TRUE)
+    filelock::lock(paste0("~/TSI/REPORTS/", basename(sub("\\.R$",".lock", Script.Name))), timeout = 0)
+}
+
+
+library(data.table)
+library(RNetCDF)
+
+####    Variables    ####
+FROM   <- "https://www.ncei.noaa.gov/data/total-solar-irradiance/access/daily/"
+DEST   <- "~/DATA/SUN/TSI_model_NOAA/"
+OUTPUT <- paste0(sub("/$","",DEST),".Rds")
+
+
+####    Get data    ####
+system(paste("wget -N -r -np -nd -nH -A .nc -P", DEST, FROM))
+
+
+####    Check if we have to parse    ####
+ncfiles <- list.files(path       = DEST,
+                      pattern    = "*.nc",
+                      recursive  = T,
+                      full.names = T )
+
+if (!file.exists(OUTPUT) | file.mtime(OUTPUT) < max(file.mtime(ncfiles))) {
+    cat("New data to parse!\n")
+} else {
+    cat("NO new data to parse!\n")
+    stop("NO new data to parse!\n")
+}
+
+
+####    Parse data    ####
+gather <- data.table()
+for (af in ncfiles) {
+
+    anc  <- open.nc(af, write  = FALSE)
+    data <- read.nc(anc, unpack = TRUE )
+    # print.nc(anc)
+
+    data <- data.table( TSI      = as.vector(data$TSI),
+                        time     = as.vector(data$time),
+                        TSI_UNC  = as.vector(data$TSI_UNC),
+                        time_low = as.vector(data$time_bnds[1,]),
+                        time_upp = as.vector(data$time_bnds[2,]) )
+
+    dateorigin <- "1610-01-01 00:00:00"
+    stopifnot( grepl( dateorigin, att.get.nc(anc, "time",      "units") ) )
+    stopifnot( grepl( dateorigin, att.get.nc(anc, "time",      "units") ) )
+    stopifnot( grepl( dateorigin, att.get.nc(anc, "time_bnds", "units") ) )
+
+    data$time     <- as.Date(    data$time,     origin = "1610-01-01 00:00:00" )
+    data$time     <- as.POSIXct( data$time ) + 12 * 3600
+    data$time_low <- as.Date(    data$time_low, origin = "1610-01-01 00:00:00" )
+    data$time_upp <- as.Date(    data$time_upp, origin = "1610-01-01 00:00:00" )
+    data$file     <- af
+
+    gather <- rbind(gather, data)
+}
+setorder(gather, time)
+
+hist( gather$TSI )
+
+plot(gather$time, gather$TSI, pch = ".")
+
+myRtools::write_RDS(object = gather,
+                    file   = OUTPUT  )
+
+tac <- Sys.time()
+cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
---
+++ prepare_TSI.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+## created on 2022-04-15
+
+#### This is for daily usage.
+## For building TSI data
+
+
+info() { echo ; echo "$(date +%F_%T) :: $* " >&1; }
+mkdir -p "$(dirname "$0")/LOGs/"
+LOG_FILE="$(dirname "$0")/LOGs/$(basename "$0")_$(date +%F_%T).log"
+ERR_FILE="$(dirname "$0")/LOGs/$(basename "$0")_$(date +%F_%T).err"
+exec  > >(tee -i "${LOG_FILE}")
+exec 2> >(tee -i "${ERR_FILE}" >&2)
+info "START :: $0 :: $* ::"
+
+
+
+sleep 1
+
+
+
+
+## end coding
+printf "%s %-10s %-10s %-50s %f\n" "$(date +"%F %H:%M:%S")" "$HOSTNAME" "$USER" "$(basename $0)" "$SECONDS"
+exit 0 
