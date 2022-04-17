53b9d647fb1be2f9f0923e73426770b65f632e9b
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-04-17 12:07:17 +0300

    Auto commit

--- DEFINITIONS.R
+++ DEFINITIONS.R
@@ -16,8 +16,20 @@ OUTPUT_NOAA_LAP <- "~/DATA/SUN/TSI_model_NOAA_LAP.Rds"
 
 
 ####    TSIS TSI from LISIRD    ################################################
-FROM_TSIS      <- "https://lasp.colorado.edu/lisird/latis/dap/tsis_tsi_6hr.csv?&format_time(yyyy-MM-dd'T'HH:mm:ss.SSSZ)"
+FROM_TSIS       <- "https://lasp.colorado.edu/lisird/latis/dap/tsis_tsi_6hr.csv?&format_time(yyyy-MM-dd'T'HH:mm:ss.SSSZ)"
 DEST_TSIS       <- "~/DATA/SUN/TSI_tsis_6hr.csv"
 OUTPUT_TSIS     <- "~/DATA/SUN/TSI_tsis_6hr.Rds"
 OUTPUT_TSIS_LAP <- "~/DATA/SUN/TSI_tsis_6hr_LAP.Rds"
+
+
+
+####    PMOD TSI from pmodwrc.ch    ############################################
+FROM_PMOD       <- "ftp://ftp.pmodwrc.ch/pub/data/irradiance/composite/DataPlots/ext_composite_42_65_1805.dat"
+DEST_PMOD       <- "~/DATA/SUN/ext_composite_42_65_1805.dat"
+OUTPUT_PMOD     <- "~/DATA/SUN/TSI_PMOD_ext_composite_42_65_1805.Rds"
+OUTPUT_PMOD_LAP <- "~/DATA/SUN/TSI_PMOD_ext_composite_42_65_1805_LAP.Rds"
+
+
+
+
 
