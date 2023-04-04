59df618e5d7e7b81785c3658c63559d3537a2118
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-04-04 10:13:19 +0300

    Commit 2023-04-04 10:13

--- TSI_NOAA_LAP.R
+++ TSI_NOAA_LAP.R
@@ -1,6 +1,5 @@
 #!/usr/bin/env Rscript
 # /* Copyright (C) 2022-2023 Athanasios Natsis <natsisphysicist@gmail.com> */
-
 #' ---
 #' title:  "TSI NOAA data preparation."
 #' author: "Natsis Athanasios"
@@ -17,7 +16,7 @@
 #' date: "`r format(Sys.time(), '%F')`"
 #' ---
 
-#+ include=FALSE
+#+ include=T, echo=F
 
 ## __ Set environment  ---------------------------------------------------------
 rm(list = (ls()[ls() != ""]))
@@ -38,66 +37,69 @@ library(caTools)
 # library(RAerosols)
 library(data.table)
 
-opts_chunk$set(comment    = NA)
+opts_chunk$set(dev        = "png")
+opts_chunk$set(comment    = NA   )
 opts_chunk$set(fig.width  = 8,
-               fig.height = 5 )
+               fig.height = 5    )
 
 source("~/TSI/DEFINITIONS.R")
-
 
 ## __ Load data  ---------------------------------------------------------------
 ASTROPY_data <- data.table(readRDS(ASTROPYdb))
 NOAA_data    <- data.table(readRDS(OUTPUT_NOAA))
 
-stop()
-
-ASTROPY_data <- ASTROPY_data[Date > TSI_START]
+## NOAA data does not extend to now
+ASTROPY_data <- ASTROPY_data[Date > TSI_START & Date <= max(NOAA_data$time)]
 NOAA_data    <- NOAA_data[   time > TSI_START]
 
-
-
-
+## remove columns
 NOAA_data[, file     := NULL ]
 NOAA_data[, time_low := NULL ]
 NOAA_data[, time_upp := NULL ]
 
-
-plot(ASTROPY_data$Date, ASTROPY_data$Dist, "l")
-
-
-#+ include=FALSE
-
-# stop()
+plot(ASTROPY_data$Date, ASTROPY_data$Dist, "l",
+     xlab = "", ylab = "Distance [au]")
 
 
-#' #### Interpolate TSI measurements to our dates ####
+#'
+#' ## Interpolate TSI measurements to our dates
+#'
 #' Make functions from TSI measurements to match out data.
+#'
 #' Interpolate between measurements only.
+#'
+#' These data are the main TSI data we use.
+#' It is completed by TSIS for the current year.
+#
+#+ include = TRUE, echo = FALSE
+
+## TSI
 tsi_fun <- approxfun(x      = NOAA_data$time,
                      y      = NOAA_data$TSI,
                      method = "linear",
                      rule   = 1,
                      ties   = mean )
-
-#+ include=FALSE
-cat("Interpolate between measurements\n")
-
-#+ include=TRUE
+## TSI uncertainty
 unc_fuc <- approxfun(x      = NOAA_data$time,
                      y      = NOAA_data$TSI_UNC,
                      method = "linear",
                      rule   = 1,
                      ties   = mean )
 
-#' Interpolate the data, we have assumed that dates from Astropy are complete (we made them).
-tsi_all      <- tsi_fun( ASTROPY_data$Date )
-unc_all      <- unc_fuc( ASTROPY_data$Date )
+#'
+#' Interpolate the data, assuming that dates from Astropy are complete (we made them).
+#'
+#+ include = TRUE, echo = FALSE
+tsi_all      <- tsi_fun(ASTROPY_data$Date)
+unc_all      <- unc_fuc(ASTROPY_data$Date)
 
+#'
 #' Compute TSI on earth using TSI at 1 au
-tsi_astropy  <- tsi_all / ( ASTROPY_data$Dist ^ 2 )
+#'
+#+ include = TRUE, echo = FALSE
+tsi_astropy  <- tsi_all / (ASTROPY_data$Dist^2)
 
-#+ include=FALSE
-#### Constructed TSI data for output --------------------------------
+##  Constructed TSI data for output  -------------------------------------------
 tsi_comb <- data.frame(
                 nominal_dates         = ASTROPY_data$Date, # Dates from SORCE extended to today
                 sun_dist              = ASTROPY_data$Dist, # Astropy sun distance not optimal
@@ -107,21 +109,12 @@ tsi_comb <- data.frame(
                 tsi_1au               = tsi_all            # TSI at 1 au
             )
 
-#+ include=FALSE
-## Some test graphs
-# pdf(file = tsigraphs)
-#     par(mar = c(2,4,2,1))
-#     pdf.options(height=5)
-#     plot(ASTROPY_data$Date, tsi_all,           "l", ylab = "TSI (Interpolated) watt/m^2")
-#     plot(ASTROPY_data$Date, tsi_astropy,       "l", ylab = "TSI True Earth (Astropy) watt/m^2")
-#     plot(ASTROPY_data$Date, unc_all,           "l", ylab = "TSI Uncertainty (Interpolated) watt/m^2")
-#     plot(ASTROPY_data$Date, ASTROPY_data$Dist, "l", ylab = "Sun-Thessaloniki Distance AU")
-# dev.off()
 
+par(mar = c(2, 4, 2, 1))
 
-#+ include=TRUE, echo=FALSE
-par(mar = c(2,4,2,1))
-#' ### Solar irradiance at 1 au ###
+#'
+#' ### Solar irradiance at 1 au
+#'
 #+ include=TRUE, echo=FALSE
 ylim = range(c( tsi_all + 0.1, tsi_all - 0.1 ), na.rm = T)
 plot( ASTROPY_data$Date, tsi_all, "l", xlab = "", ylab = "TSI (Interpolated) watt/m^2", ylim = ylim)
@@ -147,36 +140,36 @@ text(ASTROPY_data$Date[1], y = qq[1],
 text(ASTROPY_data$Date[1], y = qq[5],
      as.character(round(qq[5],1)), adj = c(0,1.5), col = 'red', lwd = 4 )
 
-pander(summary(tsi_all),digits = 8)
+pander(summary(tsi_all), digits = 8)
 
+#'
 #' ### Solar irradiance at TOA
+#'
 #+ include=TRUE, echo=FALSE
 plot(ASTROPY_data$Date, tsi_astropy,       "l", xlab = "", ylab = "TSI True Earth (Astropy) watt/m^2")
 pander(summary(tsi_astropy),digits = 8)
 
+#'
 #' ### Solar irradiance uncertainty
+#'
 #+ include=TRUE, echo=FALSE
 plot(ASTROPY_data$Date, unc_all,           "l", xlab = "", ylab = "TSI Uncertainty (Interpolated) watt/m^2")
 pander(summary(unc_all))
 
+#'
 #' ### Earth - Sun distance
+#'
 #+ include=TRUE, echo=FALSE
 plot(ASTROPY_data$Date, ASTROPY_data$Dist, "l", xlab = "", ylab = "Sun-Thessaloniki Distance AU")
 pander(summary(ASTROPY_data$Dist))
 
-#+ include=FALSE
-## Export last day of SORCE measurements to file
-# LAST_TSI_DATE = max(tsi_df$nominal_date_yyyymmdd)
-# save( LAST_TSI_DATE, file = GLOBAL_file)
 
-
-#' #### Output data for use ####
+#'
+#' ## Output data for use
+#'
 #+ include=TRUE, echo=FALSE
 myRtools::write_RDS(object = tsi_comb,
-                    file   = OUTPUT_NOAA_LAP  )
-
-#+ include=FALSE
-
+                    file   = OUTPUT_NOAA_LAP)
 
 panderOptions("table.style", 'rmarkdown')
 panderOptions("table.split.table", 100 )
@@ -184,15 +177,17 @@ panderOptions("table.continues", '')
 panderOptions("graph.fontsize", 9)
 panderOptions("table.alignment.default", "right")
 
-
-#' ### Statistics on input data ###
+#'
+#' ### Statistics on input data
+#'
 #+ include=TRUE, echo=FALSE
 pander(summary(data,   digits = 5))
 
-#' ### Statistics on output data ###
+#'
+#' ### Statistics on output data
+#'
 #+ include=TRUE, echo=FALSE
 pander(summary(tsi_comb, digits = 5))
-
 
 #' **END**
 #+ include=T, echo=F
